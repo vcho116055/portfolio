453df6c159245e035de4aa26c51463d9e01915b7
vcho116055 <vicho@ucsd.edu>
2025-04-25 17:15:14 -0700

    updated fetchJSON

--- global.js
+++ global.js
@@ -86,10 +86,12 @@ export async function fetchJSON(url) {
   try {
     // Fetch the JSON file from the given URL
     const response = await fetch(url);
+    console.log(response)
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
-    console.log(response)
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
